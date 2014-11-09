de05bf6902816fddcc9d40c18196472593463fb4
Lary Loose <n/a>
2014-11-10 00:20:25 +0100

    modified:   addons.xml 	modified:   addons.xml.md5 	modified:   plugin.video.mrskin/addon.xml 	modified:   plugin.video.mrskin/changelog.txt 	modified:   plugin.video.mrskin/default.py 	modified:   zips/plugin.video.mrskin/changelog.txt 	new file:   zips/plugin.video.mrskin/plugin.video.mrskin-0.1.2.zip

--- plugin.video.mrskin/default.py
+++ plugin.video.mrskin/default.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys, urllib, urllib2, re, xbmcplugin, xbmcgui, xbmcaddon
 
-dbg = True #False
+dbg = False 
+#dbg = True
 pluginhandle = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.mrskin')
 translation = settings.getLocalizedString
@@ -14,16 +15,66 @@ viewMode = str(settings.getSetting("viewMode"))
 
 startpage = 'http://www.mrskin.com'
 playlist = 'http://www.mrskin.com/video/playlist?t=<tid>'
+playlistPlayer = 'http://www.mrskin.com/feeds/playlistPlayer/xml?options[quality]=hd&options[id]=<pid>'
 contrex = ['hd: \'([^\']*)\'', 'file: \'([^\']*)\'', 'resources:[^<]*"file":"([^"]*)"']
 
-def index(url):
+def index():
+	addDir('Original Videos', '', 'origVideos', '')
+	addDir('Playlists', '', 'indexPlaylists', '')
+	xbmcplugin.endOfDirectory(pluginhandle)
+	
+def indexPlaylists():
+	url = startpage + '/playlists'
+	if dbg: print 'open ' + url
+	content = getUrl(url)
+	for categories in re.compile('<ul[^>]*id="video-categories"[^>]*>.*?</ul>', re.DOTALL).findall(content):
+		for href, title in re.compile('<li[^>]*>[^<]*<a[^>]*href="([^"]*)"[^>]*>[^<]*<i[^>]*></i>([^<]*)<', re.DOTALL).findall(categories):
+			if 'http' not in href: href = startpage + href
+			title = cleanTitle(title).title()
+			if dbg: print title + ' --> ' + href
+			addDir(title, href, 'showPlaylists')
+	xbmcplugin.endOfDirectory(pluginhandle)
+
+def showPlaylists(url):
+	if dbg: print 'open ' + url
+	content = getUrl(url)
+	for collection in re.compile('<div[^>]*id="video-playlist-collection">.*?</div>', re.DOTALL).findall(content):
+		for pid, pic, title in re.compile('<li[^>]*>[^<]*<a[^>]*href="[^"]*p([0-9]+).html[^"]*"[^>]*>[^<]*<img[^>]*src="([^"]*)"[^>]*alt="([^"]*)"', re.DOTALL).findall(collection):
+			if 'http' not in pic: pic = startpage + pic
+			href = playlistPlayer.replace('<pid>', pid)
+			title = cleanTitle(title)
+			if dbg: print title + ' --> ' + href + ' --> ' + pic
+			addDir(title, href, 'showPlVideos', pic)
+	for nextpage in  re.compile('<li><a[^>]*href="([^"]*)"[^>]*>&raquo;', re.DOTALL).findall(content):
+		if 'http' not in nextpage: nextpage = startpage + nextpage
+		if dbg: print 'next page ' + nextpage
+		addDir('Next Page', nextpage, 'showPlaylists')
+	xbmcplugin.endOfDirectory(pluginhandle)
+	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
+
+def showPlVideos(url):
+	if dbg: print 'open ' + url
+	content = getUrl(url)
+	for item in re.compile('<item>(.*?)</item>', re.DOTALL).findall(content):
+		title = cleanTitle(re.compile('<jwplayer:clean_title>([^<]*)</jwplayer:clean_title>', re.DOTALL).findall(item)[0])
+		desc = re.compile('<description><!\[CDATA\[([^<]*)\]\]></description>', re.DOTALL).findall(item)[0]
+		img = re.compile('<jwplayer:thumbnail>([^<]*)</jwplayer:thumbnail>', re.DOTALL).findall(item)[0]
+		href = re.compile('<jwplayer:file>([^<]*)</jwplayer:file>', re.DOTALL).findall(item)[0]
+		if 'http' not in href: href = startpage + href 
+		if dbg: print title + ' --> ' + href + ' --> ' + img
+		addLink(title, href, 'playVideo', img)
+	xbmcplugin.endOfDirectory(pluginhandle)
+	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
+
+def origVideos():
+	url = startpage + '/video'
 	if dbg: print 'open ' + url
 	content = getUrl(url)
 	for tagid, cat in re.compile('<a[^>]*data-tag="([^"]*)"[^>]*>[^<]*<i[^>]*>[^<]*</i>([^<]*)</a>', re.DOTALL).findall(content):
 		cat = cleanTitle(cat)
 		href = playlist.replace('<tid>', tagid)
 		if dbg: print cat + ' --> ' + href
-		addDir(cat, href, 'showVideos', '')
+		addDir(cat, href, 'showVideos')
 	xbmcplugin.endOfDirectory(pluginhandle)
 	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 	
@@ -34,11 +85,11 @@ def showVideos(url):
 		href = startpage + href
 		title = cleanTitle(title)
 		if dbg: print 'add link: title=' + title + ' href=' +href + ' img=' + img
-		addLink(title, href, 'playVideo', img, '')
+		addLink(title, href, 'playVideo', img)
 	for nextpage in  re.compile('<li>[^<]*<a[^>]*href="(/video/playlist[^"]*)"[^>]*data-page="[^"]*">[^<]*&raquo;[^<]*</a>[^<]*</li>', re.DOTALL).findall(content):
 		nextpage = startpage + nextpage
 		if dbg: print 'next page ' + nextpage
-		addDir('Next Page', nextpage, 'showVideos', '')
+		addDir('Next Page', nextpage, 'showVideos')
 	xbmcplugin.endOfDirectory(pluginhandle)
 	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
 
@@ -50,8 +101,11 @@ def getVideoUrl(content):
 	
 def playVideo(url):
 	if dbg: print 'play video: ' + url
-	content = getUrl(url)
-	video = getVideoUrl(content)
+	if '_pma' in url:
+		video = url
+	else:
+		content = getUrl(url)
+		video = getVideoUrl(content)
 	if video:
 	    listitem = xbmcgui.ListItem(path=video.replace('\\', ''))
 	    return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
@@ -61,8 +115,8 @@ def playVideo(url):
 def cleanTitle(title):
 		#title = re.sub('<[^>]*>', ' ', title)
 		#title = re.sub('&#\d{3};', ' ', title)
-		title = title.replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('&quot;','"').replace('&szlig;','ß').replace('&ndash;','-')
-		#title = title.replace('&Auml;','Ä').replace('&Uuml;','Ü').replace('&Ouml;','Ö').replace('&auml;','ä').replace('&uuml;','ü').replace('&ouml;','ö').replace('&nbsp;', ' ')
+		title = title.replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('&quot;','"').replace('&szlig;','ß').replace('&ndash;','-').replace('&nbsp;', ' ')
+		#title = title.replace('&Auml;','Ä').replace('&Uuml;','Ü').replace('&Ouml;','Ö').replace('&auml;','ä').replace('&uuml;','ü').replace('&ouml;','ö')
 		#title = title.replace('„','"').replace('“','"')
 		title = re.sub('\s+', ' ', title)
 		return title.strip()
@@ -86,7 +140,7 @@ def parameters_string_to_dict(parameters):
 				paramDict[paramSplits[0]] = paramSplits[1]
 	return paramDict
 
-def addLink(name, url, mode, iconimage, fanart):
+def addLink(name, url, mode, iconimage, fanart=''):
 	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode)
 	liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
 	liz.setInfo( type="Video", infoLabels={ "Title": name } )
@@ -94,7 +148,7 @@ def addLink(name, url, mode, iconimage, fanart):
 	if useThumbAsFanart: liz.setProperty('fanart_image', fanart)
 	return xbmcplugin.addDirectoryItem(handle=pluginhandle, url=u, listitem=liz)
 
-def addDir(name, url, mode, iconimage):
+def addDir(name, url, mode, iconimage=''):
 	#name = '* ' + name
 	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode)
 	liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
@@ -109,4 +163,8 @@ if type(url) == type(str()): url = urllib.unquote_plus(url)
 
 if mode == 'showVideos': showVideos(url)
 elif mode == 'playVideo': playVideo(url)
-else: index(startpage+'/video')
+elif mode == 'origVideos': origVideos()
+elif mode == 'indexPlaylists': indexPlaylists()
+elif mode == 'showPlaylists': showPlaylists(url)
+elif mode == 'showPlVideos': showPlVideos(url)
+else: index()

b526cc39c0909f205340d64383377637a9bc7a38
Lary Loose <n/a>
2016-03-18 16:40:54 +0100

    modified:   addons.xml 	modified:   addons.xml.md5 	modified:   plugin.video.mrskin/addon.xml 	modified:   plugin.video.mrskin/changelog.txt 	modified:   plugin.video.mrskin/default.py 	modified:   zips/plugin.video.mrskin/changelog.txt 	new file:   zips/plugin.video.mrskin/plugin.video.mrskin-0.1.3.zip

--- plugin.video.mrskin/default.py
+++ plugin.video.mrskin/default.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys, urllib, urllib2, re, xbmcplugin, xbmcgui, xbmcaddon
 
-dbg = False 
-#dbg = True
+#dbg = False 
+dbg = True
 pluginhandle = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.mrskin')
 translation = settings.getLocalizedString
@@ -14,9 +14,9 @@ if maxViewPages == 0: maxViewPages = 1
 viewMode = str(settings.getSetting("viewMode"))
 
 startpage = 'http://www.mrskin.com'
-playlist = 'http://www.mrskin.com/video/playlist?t=<tid>'
-playlistPlayer = 'http://www.mrskin.com/feeds/playlistPlayer/xml?options[quality]=hd&options[id]=<pid>'
-contrex = ['hd: \'([^\']*)\'', 'file: \'([^\']*)\'', 'resources:[^<]*"file":"([^"]*)"']
+playlisturl = 'http://www.mrskin.com/playlist/<pid>.xml'
+contrex = ['"file":"([^"]+-hd[^"]*)"', '"file":"([^"]+-sd[^"]*)"', '"file":"([^"]+)"']
+userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
 
 def index():
 	addDir('Original Videos', '', 'origVideos', '')
@@ -27,66 +27,72 @@ def indexPlaylists():
 	url = startpage + '/playlists'
 	if dbg: print 'open ' + url
 	content = getUrl(url)
-	for categories in re.compile('<ul[^>]*id="video-categories"[^>]*>.*?</ul>', re.DOTALL).findall(content):
-		for href, title in re.compile('<li[^>]*>[^<]*<a[^>]*href="([^"]*)"[^>]*>[^<]*<i[^>]*></i>([^<]*)<', re.DOTALL).findall(categories):
-			if 'http' not in href: href = startpage + href
-			title = cleanTitle(title).title()
-			if dbg: print title + ' --> ' + href
-			addDir(title, href, 'showPlaylists')
+	addDir('Last Updates', url, 'showPlaylists')
+	for href, cat in re.compile('<a[^>]*class=\'filter-link\'[^>]*href=\'([^\']+)\'[^>]*>[^<]*<span[^>]*class=\'category\'[^>]*>([^<]*)</span>', re.DOTALL).findall(content):
+		cat = cleanTitle(cat)
+		if 'http' not in href: href = startpage + href
+		if dbg: print cat, href
+		addDir(cat, href, 'showPlaylists')
 	xbmcplugin.endOfDirectory(pluginhandle)
 
 def showPlaylists(url):
 	if dbg: print 'open ' + url
 	content = getUrl(url)
-	for collection in re.compile('<div[^>]*id="video-playlist-collection">.*?</div>', re.DOTALL).findall(content):
-		for pid, pic, title in re.compile('<li[^>]*>[^<]*<a[^>]*href="[^"]*p([0-9]+).html[^"]*"[^>]*>[^<]*<img[^>]*src="([^"]*)"[^>]*alt="([^"]*)"', re.DOTALL).findall(collection):
-			if 'http' not in pic: pic = startpage + pic
-			href = playlistPlayer.replace('<pid>', pid)
-			title = cleanTitle(title)
-			if dbg: print title + ' --> ' + href + ' --> ' + pic
-			addDir(title, href, 'showPlVideos', pic)
-	for nextpage in  re.compile('<li><a[^>]*href="([^"]*)"[^>]*>&raquo;', re.DOTALL).findall(content):
-		if 'http' not in nextpage: nextpage = startpage + nextpage
+	for href, img, title in re.compile('<a[^>]*href="([^"]+)">[^<]*<img[^>]*class="img-responsive"[^>]*src="([^"]*)"[^>]*>.*?<[^>]*class=["\']title["\'][^>]*>([^<]+)<', re.DOTALL).findall(content):
+		href = startpage + href
+		title = cleanTitle(title)
+		if dbg: print 'add link: title=' + title + ' href=' +href + ' img=' + img
+		addDir(title, href, 'showPlVideos', img)
+	for nextpage in  re.compile('<span[^>]*class=\'next\'>[^<]*<a[^>]*rel="next"[^>]*href="([^"]+)"[^>]*>', re.DOTALL).findall(content):
+		nextpage = startpage + nextpage
 		if dbg: print 'next page ' + nextpage
 		addDir('Next Page', nextpage, 'showPlaylists')
 	xbmcplugin.endOfDirectory(pluginhandle)
-	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
+	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')	
 
 def showPlVideos(url):
+	if dbg: print 'parsing ' + url
+	url = playlisturl.replace('<pid>', find('p([0-9]+)$', url))
 	if dbg: print 'open ' + url
 	content = getUrl(url)
+	print content
 	for item in re.compile('<item>(.*?)</item>', re.DOTALL).findall(content):
-		title = cleanTitle(re.compile('<jwplayer:clean_title>([^<]*)</jwplayer:clean_title>', re.DOTALL).findall(item)[0])
-		desc = re.compile('<description><!\[CDATA\[([^<]*)\]\]></description>', re.DOTALL).findall(item)[0]
-		img = re.compile('<jwplayer:thumbnail>([^<]*)</jwplayer:thumbnail>', re.DOTALL).findall(item)[0]
-		href = re.compile('<jwplayer:file>([^<]*)</jwplayer:file>', re.DOTALL).findall(item)[0]
+		bitrate = 0
+		title = cleanTitle(find('<title>([^<]*)</title>', item))
+		img = find('<jwplayer:thumbnail>([^<]*)</jwplayer:thumbnail>', item)
+		for ibrate, ihref in re.compile('<media:content bitrate="([0-9]+)" url="([^"]+)"', re.I).findall(item):
+			#print ibrate, ihref
+			if int(ibrate) > bitrate: 
+				bitrate = int(ibrate)
+				href = ihref
 		if 'http' not in href: href = startpage + href 
 		if dbg: print title + ' --> ' + href + ' --> ' + img
 		addLink(title, href, 'playVideo', img)
 	xbmcplugin.endOfDirectory(pluginhandle)
 	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
-
+	
 def origVideos():
 	url = startpage + '/video'
 	if dbg: print 'open ' + url
 	content = getUrl(url)
-	for tagid, cat in re.compile('<a[^>]*data-tag="([^"]*)"[^>]*>[^<]*<i[^>]*>[^<]*</i>([^<]*)</a>', re.DOTALL).findall(content):
+	addDir('Last Updates', url, 'showVideos')
+	for href, cat in re.compile('<a[^>]*class=\'filter-link\'[^>]*href=\'([^\']+)\'[^>]*>[^<]*<span[^>]*class=\'category\'[^>]*>([^<]*)</span>', re.DOTALL).findall(content):
 		cat = cleanTitle(cat)
-		href = playlist.replace('<tid>', tagid)
-		if dbg: print cat + ' --> ' + href
+		if 'http' not in href: href = startpage + href
+		if dbg: print cat, href
 		addDir(cat, href, 'showVideos')
 	xbmcplugin.endOfDirectory(pluginhandle)
 	if forceViewMode: xbmc.executebuiltin('Container.SetViewMode('+viewMode+')')
-	
+
 def showVideos(url):
 	if dbg: print 'open ' + url
 	content = getUrl(url)
-	for href, img, title in re.compile('<a[^>]*href="([^"]*)"[^>]*class="[^"]*video[^"]*plain[^"]*"[^>]*>[^<]*<img[^>]*src="([^"]*)"[^>]*alt="([^"]*)"[^>]*>', re.DOTALL).findall(content):
+	for href, img, title in re.compile('<a[^>]*href="([^"]+)">[^<]*<img[^>]*class="img-responsive"[^>]*src="([^"]*)"[^>]*>.*?<[^>]*class=["\']title["\'][^>]*>([^<]+)<', re.DOTALL).findall(content):
 		href = startpage + href
 		title = cleanTitle(title)
 		if dbg: print 'add link: title=' + title + ' href=' +href + ' img=' + img
 		addLink(title, href, 'playVideo', img)
-	for nextpage in  re.compile('<li>[^<]*<a[^>]*href="(/video/playlist[^"]*)"[^>]*data-page="[^"]*">[^<]*&raquo;[^<]*</a>[^<]*</li>', re.DOTALL).findall(content):
+	for nextpage in  re.compile('<span[^>]*class=\'next\'>[^<]*<a[^>]*rel="next"[^>]*href="([^"]+)"[^>]*>', re.DOTALL).findall(content):
 		nextpage = startpage + nextpage
 		if dbg: print 'next page ' + nextpage
 		addDir('Next Page', nextpage, 'showVideos')
@@ -101,33 +107,36 @@ def getVideoUrl(content):
 	
 def playVideo(url):
 	if dbg: print 'play video: ' + url
-	if '_pma' in url:
+	if '.mp4' in url:
 		video = url
 	else:
 		content = getUrl(url)
 		video = getVideoUrl(content)
 	if video:
-	    listitem = xbmcgui.ListItem(path=video.replace('\\', ''))
-	    return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+		video = video + '|User-Agent=' + userAgent + '&Referer=' + startpage
+		listitem = xbmcgui.ListItem(path=video.replace('\\', ''))
+		return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 	else:
 	    xbmc.executebuiltin('Notification(Video not found., 5000)')
 
 def cleanTitle(title):
-		#title = re.sub('<[^>]*>', ' ', title)
-		#title = re.sub('&#\d{3};', ' ', title)
-		title = title.replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('&quot;','"').replace('&szlig;','ß').replace('&ndash;','-').replace('&nbsp;', ' ')
-		#title = title.replace('&Auml;','Ä').replace('&Uuml;','Ü').replace('&Ouml;','Ö').replace('&auml;','ä').replace('&uuml;','ü').replace('&ouml;','ö')
-		#title = title.replace('„','"').replace('“','"')
-		title = re.sub('\s+', ' ', title)
-		return title.strip()
+	title = title.replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('&quot;','"').replace('&szlig;','ß').replace('&ndash;','-').replace('&nbsp;', ' ')
+	title = re.sub('\s+', ' ', title)
+	return title.strip()
 
 def getUrl(url):
 	req = urllib2.Request(url)
-	req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0')
+	req.add_header('User-Agent', userAgent)
+	req.add_header('Referer', url)
 	response = urllib2.urlopen(req, timeout=30)
 	link = response.read()
 	response.close()
 	return link
+
+def find(rex, string):
+	match = re.search(rex, string, re.S|re.I|re.DOTALL)
+	if match: return match.group(1)
+	else: return ''
 
 def parameters_string_to_dict(parameters):
 	''' Convert parameters encoded in a URL to a dict. '''

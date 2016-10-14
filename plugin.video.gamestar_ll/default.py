0c4577f9fb5a9213673ba2f8970d430e23b0c36c
Lary Loose <n/a>
2016-10-14 23:46:11 +0200

    modified:   addons.xml 	modified:   addons.xml.md5 	modified:   plugin.video.gamestar_ll/addon.xml 	modified:   plugin.video.gamestar_ll/changelog.txt 	modified:   plugin.video.gamestar_ll/default.py 	modified:   zips/plugin.video.gamestar_ll/changelog.txt 	new file:   zips/plugin.video.gamestar_ll/plugin.video.gamestar_ll-0.1.6.zip

--- plugin.video.gamestar_ll/default.py
+++ plugin.video.gamestar_ll/default.py
@@ -7,8 +7,7 @@ pluginhandle = int(sys.argv[1])
 itemcnt = 0
 baseurl = 'http://www.gamestar.de'
 channelurl = 'http://www.gamestar.de/videos/video-kanaele/'
-getvideourl = 'http://www.gamestar.de/_misc/videos/portal/getVideoUrl.cfm?premium=0&videoId='
-googleresize = 'http://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&url='
+getvideourl = 'http://www.gamestar.de/_misc/videos/portal/getVideoUrl.cfm?premium=1&videoId='
 settings = xbmcaddon.Addon(id='plugin.video.gamestar_ll')
 maxitems = (int(settings.getSetting("items_per_page"))+1)*10
 forceMovieViewMode = settings.getSetting("forceMovieViewMode") == 'true'
@@ -16,51 +15,42 @@ useThumbAsFanart = settings.getSetting("useThumbAsFanart") == 'true'
 hirespix = settings.getSetting("useHighresPix") == 'true'
 movieViewMode = str(settings.getSetting("movieViewMode"))
 
+premiumrex = re.compile('^Die Redaktion|^Raumschiff GameStar|^GameStar TV', re.I)
+
 premium = False
 dbg = False
-
-cats = [
-	('http://www.gamestar.de/videos/latest/','Neueste Videos','',''),
-	('http://www.gamestar.de/videos/popular/','Meist gesehen','',''),
-	('http://www.gamestar.de/videos/news,100/','News','Von Montag bis Freitag immer mittags berichten wir in unserer News-Show über die wichtigsten Spiele-Themen des Tages.','http://images.gamestar.de/images/idgwpgsgp/bdb/2558457/b144x81.jpg'),
-	('http://www.gamestar.de/videos/was-ist-,96/','Was ist ...?','In »Was ist…?« präsentieren wir Indie-Hits, Geheimtipps und andere Spiele-Kleinode mit kommentierten Spielszenen.',''),
-	('http://www.gamestar.de/videos/feedback,99/','Feedback','In Feedback beantwortet unser Team regelmäßig Fragen der Community und plaudert mit Moderator Andre Peschke aus dem Nähkästchen.',''),
-	('http://www.gamestar.de/videos/kino-und-dvd,26/','Kino und DVD','Aktuelle Trailer zu Kinofilmen und DVD-Neuerscheinungen.','http://images.gamestar.de/images/idgwpgsgp/bdb/2334506/b144x81.jpg'),
-	('http://www.gamestar.de/videos/gamewatch,97/','Gamewatch','Neue Trailer, Gameplay-Videos oder Live-Demos.',''),
-	('http://www.gamestar.de/videos/public-viewing,37/','Public Viewing','Neue Spiele ausführlich angespielt und vorgestellt','http://images.gamestar.de/images/idgwpgsgp/bdb/2121485/b144x81.jpg'),
-	('http://www.gamestar.de/index.cfm?pid=1589&ci=9','Quickplay','Alle Trailer aus dem Action-Genre mit den Unterrubriken Ego-Shooter, Action-Adventures, Flugsimulationen und anderen.','http://images.gamestar.de/images/idgwpgsgp/bdb/2016676/b144x81.jpg'),
-	('http://www.gamestar.de/videos/candyland,102/','Candyland','In diesem Kanal zeigen wir in erster Linie Grafikvergleiche zu PC und Konsolenspielen.','http://images.gamestar.de/images/idgwpgsgp/bdb/2557236/b144x81.jpg')
-]
 
 def CATEGORIES():
 	if dbg: print channelurl
 	data = getUrl(channelurl)
-	#if dbg: print data	
-	for url, name, desc, img in cats:
-		if desc: addDir(colorstr(name, 'blue') + ' - ' + desc, url, 1, renamepic(img), True)
-		else: addDir(colorstr(name, 'blue'), url, 1, renamepic(img), True)
-	for url, img, title in re.findall('<td[^>]*class="itemtext"[^>]*>[^<]*<a[^>]*href="([^"]*)"[^>]*class="imglink"[^>]*>[^<]*<img[^>]*src="([^"]*)"[^>]*>(.*?)</td>', data, re.S|re.I):
-		name = clean(re.findall('(<a.*?)</a>', title, re.S|re.I)[0])
-		desc = clean(re.findall('(<div.*?)</div>', title, re.S|re.I)[0])
-		if not premium and ('Premium' in desc or name == 'Die Redaktion' or name == 'Raumschiff GameStar' or name == 'GameStar TV' ): continue
+	#print data
+	list = {}
+	rex = re.compile('<a[^>]*href="([^"]*)"[^>]*title="([^"]*)"[^>]*>[^<]*<img[^>]*>[^<]*<noscript>[^<]*<img[^>]*src="([^"]*)"', re.S|re.I)
+	for url, title, img in rex.findall(data):
+		if not premium and premiumrex.match(title): continue
 		if 'http' not in url: url = baseurl + url
 		if 'http' not in img: img = 'http:' + img
-		#if dbg: print url, name, img
-		addDir(colorstr(name, 'blue') + ' - ' + desc, url, 1, renamepic(img), True)
+		if title in list: continue
+		list[title] = ''
+		if dbg: print url, title, img
+		addDir(colortitle(title, 'blue'), url, 1, renamepic(img), True)
 	xbmc.executebuiltin("Container.SetViewMode(400)")
 
 def INDEX(url):
 	if dbg: print url
 	global itemcnt
 	data = getUrl(url)
-	for url, img, title in re.findall('<div[^>]*class="videoPreview"[^>]*>[^<]*<a[^>]*class="imageLink"[^>]*href="([^"]*)"[^>]*>[^<]*<img[^>]*src="([^"]*)"[^>]*(?:title|alt)="([^"]*)"[^>]*>', data, re.S|re.I):
+	#print data
+	rex = re.compile('<a[^>]*href="([^"]*)"[^>]*title="([^"]*)"[^>]*>[^<]*<img[^>]*>[^<]*<noscript>[^<]*<img[^>]*src="([^"]*)"', re.S|re.I)
+	for url, title, img in rex.findall(data):
+		if not premium and premiumrex.match(title): continue 
 		if 'http' not in url: url =  baseurl + url
+		if 'http' not in img: img = 'http:' + img
 		title = clean(filter(title))
-		title = re.sub(r'(^.*?) - ', r'[COLOR=blue]\g<1>[/COLOR] - ', title)
 		if dbg: print url, img, title
-		addLink(title, url, 2, renamepic(img), renamepic(img, True))
+		addLink(colortitle(title, 'blue'), url, 2, renamepic(img), renamepic(img, True))
 		itemcnt = itemcnt + 1
-	nextPage = re.findall('<a[^>]*href="([^"]*)"[^>]*>[^<]*<img[^>]*src="[^"]*btnNextPage.png', data, re.S)
+	nextPage = re.findall('<a[^>]*href="([^"]*)"[^>]*>mehr anzeigen', data, re.S)
 	if nextPage:
 		url = nextPage[0]
 		if 'http' not in url: url =  baseurl + url
@@ -78,8 +68,8 @@ def PLAYLINK(url):
 		listitem = xbmcgui.ListItem(path=url)
 		return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)		
 			
-def colorstr(s, color):
-	return '[COLOR=' + color + ']' + s + '[/COLOR]'
+def colortitle(s, color):
+	return re.sub(r'(^.*?(?: -|:))', r'[COLOR=blue]\g<1>[/COLOR]', s)
 
 def clean(s):
 	try: s = htmlparser.unescape(s)
@@ -96,8 +86,7 @@ def renamepic(p, fanart=False):
 	if not p: return p
 	if fanart: p = re.sub('b144x81', 'pic', p)
 	if hirespix: p = re.sub('b144x81', '450x', p)
-	if dbg: print googleresize + p
-	return googleresize + p
+	return p
 
 def filter(s):
 	return re.sub('^Video: ', '', s)

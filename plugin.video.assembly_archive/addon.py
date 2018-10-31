3aaf040d8f4d4deeb7b664770f8ac9e8c65f8aef
Lary Loose <n/a>
2018-10-31 21:00:57 +0100

    modified:   addons.xml 	modified:   addons.xml.md5 	modified:   plugin.video.assembly_archive/addon.py 	modified:   plugin.video.assembly_archive/addon.xml 	modified:   plugin.video.assembly_archive/changelog.txt 	modified:   zips/plugin.video.assembly_archive/changelog.txt 	new file:   zips/plugin.video.assembly_archive/plugin.video.assembly_archive-0.1.3.zip

--- plugin.video.assembly_archive/addon.py
+++ plugin.video.assembly_archive/addon.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon,base64
+import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon
 
 pluginhandle = int(sys.argv[1])
 settings = xbmcaddon.Addon(id='plugin.video.assembly_archive')
 translation = settings.getLocalizedString
 forceViewMode = settings.getSetting("force_viewmode") == "true"
-
+baseurl = 'http://archive.assembly.org/'
+	
 def index():
-		content = getUrl('http://archive.assembly.org/')
-		years = re.compile('class="mediacategory".*?href="(.*?)"[^<]*><span>(.*?)</span>', re.DOTALL).findall(content)
-		for i in range(0, len(years), 1):
-			addDir(years[i][1],years[i][0],'showFolders','')
+		content = getUrl(baseurl)
+		for href,year in re.compile('class="mediacategory".*?href="([^"]+)"[^>]*><span>(.*?)</span>', re.DOTALL).findall(content):
+			addDir(year,href,'showFolders','')
 		xbmcplugin.endOfDirectory(pluginhandle)
 
 def showFolders(url):
 		content = getUrl(url)
-		categories = re.compile('class="mediacategory".*?href="(.*?)"[^<]*>(.*?)</a>', re.DOTALL).findall(content)
-		for i in range(0, len(categories), 1):
-			addDir(cleanTitle(categories[i][1]),categories[i][0],'showVideos','')
+		for href,category in re.compile('class="mediacategory".*?href="([^"]+)"[^>]*>(.*?)</a>', re.DOTALL).findall(content):
+			addDir(cleanTitle(category),href,'showVideos','')
 		xbmcplugin.endOfDirectory(pluginhandle)
 
 def showVideos(url):
 		content = getUrl(url)
-		videos = re.compile('class="video".*?href="(.*?)"[^<]*>[^<]*<img[^<]*src="(.*?)"[^<]*alt="(.*?)"[^<]*>[^<]*<span class="by">(.*?)</span>', re.DOTALL).findall(content)
-		for i in range(0, len(videos), 1):
-			print(videos[i])
-			addLink('"' + cleanTitle(videos[i][2]) + '" by ' + cleanTitle(videos[i][3]), videos[i][0], 'playVideo', videos[i][1])
+		for href,img,name,by in re.compile('class="video".*?href="(.*?)"[^<]*>[^<]*<img[^<]*src="(.*?)"[^<]*alt="(.*?)"[^<]*>[^<]*<span class="by">(.*?)</span>', re.DOTALL).findall(content):
+			addLink(cleanTitle(name + ' by ' + by), href, 'playVideo', img)
 		if forceViewMode:
 			xbmc.executebuiltin('Container.SetViewMode(500)')
 		xbmcplugin.endOfDirectory(pluginhandle)
@@ -36,19 +33,17 @@ def cleanTitle(text):
 		text = re.sub('&amp;', '&', text)
 		text = re.sub('&.*?;', ' ', text)
 		text = re.sub('\s+', ' ', text)
-		text = re.sub('^\s|\s$', '', text)
-		return text
+		return re.sub('^\s|\s$', '', text).strip()
 
 def playVideo(url):
-        content = getUrl(url)
-        match = re.compile('src="http://www.youtube.com/embed/(.+?)\\?', re.DOTALL).findall(content)
-        youtubeID = match[0]
-        if xbmc.getCondVisibility("System.Platform.xbox") == True:
-          fullData = "plugin://video/YouTube/?path=/root/video&action=play_video&videoid=" + youtubeID
-        else:
-          fullData = "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid=" + youtubeID
-        listitem = xbmcgui.ListItem(path=fullData)
-        return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+		content = getUrl(url)
+		for youtubeID in re.compile('<[^>]*id="ytplayerembed"[^>]*src="[^"]*youtube.com/embed/([^"]+)"', re.DOTALL).findall(content):
+			if xbmc.getCondVisibility("System.Platform.xbox") == True:
+				fullData = "plugin://video/YouTube/?path=/root/video&action=play_video&videoid=" + youtubeID
+			else:
+				fullData = "plugin://plugin.video.youtube/play/?video_id=" + youtubeID
+			listitem = xbmcgui.ListItem(path=fullData)
+			return xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 
 def getUrl(url):
         req = urllib2.Request(url)
@@ -71,32 +66,23 @@ def parameters_string_to_dict(parameters):
 
 def addLink(name,url,mode,iconimage):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
-        ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
         liz.setProperty('IsPlayable', 'true')
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
-        return ok
+        return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
 
 def addDir(name,url,mode,iconimage):
 		u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
-		ok=True
 		liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
 		liz.setInfo( type="Video", infoLabels={ "Title": name } )
-		ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-		return ok
+		return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
 
 params=parameters_string_to_dict(sys.argv[2])
 mode=params.get('mode')
 url=params.get('url')
-if type(url)==type(str()):
-  url=urllib.unquote_plus(url)
+if type(url)==type(str()): url=urllib.unquote_plus(url)
 
-if mode == 'showFolders':
-    showFolders(url)
-elif mode == 'showVideos':
-    showVideos(url)
-elif mode == 'playVideo':
-    playVideo(url)
-else:
-    index()
+if mode == 'showFolders': showFolders(url)
+elif mode == 'showVideos': showVideos(url)
+elif mode == 'playVideo': playVideo(url)
+else: index()
